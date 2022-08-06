2de9e90c5cd912a78710d4dea1d34073be49331b
Dan Muendo <danwalker254254@gmail.com>
2022-08-06 09:50:32 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name : "dan",
+    streetAddress :"Los Santos",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key] : value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   employee[key] = value;
+   return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+  const employeeNew = {...employee};
+  delete employeeNew[key];
+  return employeeNew; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
